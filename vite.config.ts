3465e23eb183671b906b6e2ced5a5625eead9ddb
Govind Kumar Soni <sonigovindo7@gmail.com>
2026-07-22 01:40:14 +0530

    Fix GitHub Pages root loading

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    manifest: 'manifest.json',
+  },
   test: {
     environment: 'jsdom',
     globals: true,
